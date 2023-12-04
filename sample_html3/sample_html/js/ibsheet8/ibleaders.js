fe1aba6fed656e57b2865d4f4b7c190eeeb19e47
cki6090 <52525423+cki6090@users.noreply.github.com>
2023-12-05 07:19:42 +0900

    20231205

---
+++ sample_html3/sample_html/js/ibsheet8/ibleaders.js
@@ -0,0 +1,25 @@
+/*
+ * https://www.ibsheet.com
+ * Call 1644-5615
+ *
+ *  Copyright 2015 IB LEADERS CO. LTD.
+ */
+var ibleaders = ibleaders || {
+  /**
+   * [ licenseType ]
+   * - enum [ "file", "value" ] default 'value'
+   * - file로 사용할 경우 licenseType 을 명시하고 해당 프로퍼티 value로 "file" 을 설정한다.
+   */
+  licenseType: 'value',
+
+  /**
+   * [ license ]
+   * - licenseType이 "value" 일 경우 라이선스 값을 기입한다.
+   * - licenseType이 "file" 일 경우 라이선스 파일 명을 기입한다.
+   * - IBSheet8는 localhost, 127.0.0.1까지 검사함
+   * Product: IBSheet7, IBSheet8, IBChart, IBControlX
+   * Created: 2022-04-29
+   * Expired: 180day later (버전은 545일)
+   */
+  license: "W2FtSztPKCByZTY3YjJxbn8RY1wzUjN0JyZmZSl6OXtmclcnRTJTJmhoMTV/M2U+fmI7DWUOKV4gImR/NWYzaCEqfGxGIgd8E3AwPXo1eyR0aj4xd1YmWz5RPCkvZTFpIyJkcDVtSCpOcRQsJG0sOHZoKTFpNXBbNRtkUyxvLjZ/cX5hNWwzfl5jESlJN3s5eHdyaywteiB+E3RGME0pdicneW4sIWstOW5ZaR9EP1xtZHdhNGsHUxhzbUw7QXpXJT5nYjNkMA=="  
+};
